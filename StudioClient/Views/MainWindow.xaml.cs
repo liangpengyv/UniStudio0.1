e042a9c9d5182a5641f5ff09d0d07c01280a15c5
liangpengyv <827119229@qq.com>
2019-10-09 15:25:21 +0800

    修改默认 Activities 加载逻辑

--- StudioClient/Views/MainWindow.xaml.cs
+++ StudioClient/Views/MainWindow.xaml.cs
@@ -81,42 +81,47 @@ namespace StudioClient.Views
         }
 
         /// <summary>
-        /// 添加 Activities 工具项模块内容
+        /// 更新 Activities 工具项模块内容
         /// </summary>
-        private void AddActivitiesToolBoxContent()
+        private void UpdateActivitiesToolBoxContent()
         {
-            // 创建工具箱控件
-            ToolboxControl ctrl = new ToolboxControl();
+            // TODO 更新 Activities 工具箱
+            // 根据传入的项目配置文件，更新当前项目需要加载的 Activities 项目
+            // 注意默认需要加载的 Activities 的处理
 
-            // 创建分类
-            ToolboxCategory control = new ToolboxCategory("Control");
-            ToolboxCategory flowchart = new ToolboxCategory("Flowchart");
-            ToolboxCategory stateMachine = new ToolboxCategory("State Machine");
-            ToolboxCategory custom = new ToolboxCategory("Custom");
+            // Old
+            //// 创建工具箱控件
+            //ToolboxControl ctrl = new ToolboxControl();
 
-            // 为每个分类添加一些控件
-            // Control
-            control.Add(new ToolboxItemWrapper("System.Activities.Statements.Sequence", typeof(Sequence).Assembly.FullName, null, "Sequence"));
-            control.Add(new ToolboxItemWrapper("System.Activities.Statements.Assign", typeof(Assign).Assembly.FullName, null, "Assign"));
-            control.Add(new ToolboxItemWrapper("System.Activities.Statements.If", typeof(If).Assembly.FullName, null, "If"));
-            control.Add(new ToolboxItemWrapper("System.Activities.Statements.DoWhile", typeof(DoWhile).Assembly.FullName, null, "DoWhile"));
-            control.Add(new ToolboxItemWrapper("System.Activities.Statements.While", typeof(While).Assembly.FullName, null, "While"));
-            control.Add(new ToolboxItemWrapper("System.Activities.Statements.WriteLine", typeof(WriteLine).Assembly.FullName, null, "WriteLine"));
-            // Flowchart
-            flowchart.Add(new ToolboxItemWrapper("System.Activities.Statements.Flowchart", typeof(Flowchart).Assembly.FullName, null, "Flowchart"));
-            flowchart.Add(new ToolboxItemWrapper("System.Activities.Statements.FlowDecision", typeof(FlowDecision).Assembly.FullName, null, "Flow Decision"));
-            // State Machine
-            stateMachine.Add(new ToolboxItemWrapper("System.Activities.Statements.StateMachine", typeof(StateMachine).Assembly.FullName, null, "State Machine"));
-            stateMachine.Add(new ToolboxItemWrapper("System.Activities.Statements.State", typeof(System.Activities.Statements.State).Assembly.FullName, null, "State"));
-            // Custom
-            custom.Add(new ToolboxItemWrapper("CustomActivities.SendMail.SendMail", typeof(CustomActivities.SendMail.SendMail).Assembly.FullName, null, "SendMail(temp test)"));
+            //// 创建分类
+            //ToolboxCategory control = new ToolboxCategory("Control");
+            //ToolboxCategory flowchart = new ToolboxCategory("Flowchart");
+            //ToolboxCategory stateMachine = new ToolboxCategory("State Machine");
+            //ToolboxCategory custom = new ToolboxCategory("Custom");
 
-            // 把每个分类添加到工具箱
-            ctrl.Categories.Add(control);
-            ctrl.Categories.Add(flowchart);
-            ctrl.Categories.Add(stateMachine);
-            ctrl.Categories.Add(custom);
-            _activities.Content = ctrl;
+            //// 为每个分类添加一些控件
+            //// Control
+            //control.Add(new ToolboxItemWrapper("System.Activities.Statements.Sequence", typeof(Sequence).Assembly.FullName, null, "Sequence"));
+            //control.Add(new ToolboxItemWrapper("System.Activities.Statements.Assign", typeof(Assign).Assembly.FullName, null, "Assign"));
+            //control.Add(new ToolboxItemWrapper("System.Activities.Statements.If", typeof(If).Assembly.FullName, null, "If"));
+            //control.Add(new ToolboxItemWrapper("System.Activities.Statements.DoWhile", typeof(DoWhile).Assembly.FullName, null, "DoWhile"));
+            //control.Add(new ToolboxItemWrapper("System.Activities.Statements.While", typeof(While).Assembly.FullName, null, "While"));
+            //control.Add(new ToolboxItemWrapper("System.Activities.Statements.WriteLine", typeof(WriteLine).Assembly.FullName, null, "WriteLine"));
+            //// Flowchart
+            //flowchart.Add(new ToolboxItemWrapper("System.Activities.Statements.Flowchart", typeof(Flowchart).Assembly.FullName, null, "Flowchart"));
+            //flowchart.Add(new ToolboxItemWrapper("System.Activities.Statements.FlowDecision", typeof(FlowDecision).Assembly.FullName, null, "Flow Decision"));
+            //// State Machine
+            //stateMachine.Add(new ToolboxItemWrapper("System.Activities.Statements.StateMachine", typeof(StateMachine).Assembly.FullName, null, "State Machine"));
+            //stateMachine.Add(new ToolboxItemWrapper("System.Activities.Statements.State", typeof(System.Activities.Statements.State).Assembly.FullName, null, "State"));
+            //// Custom
+            //custom.Add(new ToolboxItemWrapper("CustomActivities.SendMail.SendMail", typeof(CustomActivities.SendMail.SendMail).Assembly.FullName, null, "SendMail(temp test)"));
+
+            //// 把每个分类添加到工具箱
+            //ctrl.Categories.Add(control);
+            //ctrl.Categories.Add(flowchart);
+            //ctrl.Categories.Add(stateMachine);
+            //ctrl.Categories.Add(custom);
+            //_activities.Content = ctrl;
         }
 
         /// <summary>
@@ -215,8 +220,8 @@ namespace StudioClient.Views
             // 添加到文档窗口
             documentWindow.Activate();
 
-            // 添加 Activities 工具箱内容
-            AddActivitiesToolBoxContent();
+            // 更新 Activities 工具箱内容
+            UpdateActivitiesToolBoxContent();
 
             // 已经有打开的具体项目实例，更新一些状态
             _appMenu.CanClose = true;
@@ -310,8 +315,8 @@ namespace StudioClient.Views
                 // 添加“主流程”到文档窗口
                 mainDocumentWindow?.Activate();
 
-                // 添加 Activities 工具箱内容
-                AddActivitiesToolBoxContent();
+                // 更新 Activities 工具箱内容
+                UpdateActivitiesToolBoxContent();
 
                 // 已经有打开的具体项目实例，更新一些状态
                 _appMenu.CanClose = true;
@@ -338,7 +343,7 @@ namespace StudioClient.Views
         {
             // 清空当前项目实例的资源
             _projectNodeList.RootItem = null;
-            _activities.Content = null;
+            //_activities.Content = null;
 
             // 关闭已打开的工作流设计器窗口
             _dockSite.CloseAllDocuments();
