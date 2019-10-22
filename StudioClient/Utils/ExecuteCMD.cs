e5c28151304fdf764c5fa0920e6cc031cc84db4e
liangpengyv <827119229@qq.com>
2019-10-22 12:11:58 +0800

    抽取 ExecuteCMD 工具类

---
+++ StudioClient/Utils/ExecuteCMD.cs
@@ -0,0 +1,56 @@
+﻿using StudioClient.Model;
+using System.Diagnostics;
+
+namespace StudioClient.Utils
+{
+    class ExecuteCMD
+    {
+        /// <summary>
+        /// 执行 CMD 命令，处理执行结果
+        /// </summary>
+        /// <param name="strCMD"></param>
+        /// <returns></returns>
+        public static CmdExecuteResultModel Handle(string strCMD)
+        {
+            Process cmdProcess = new Process();
+            cmdProcess.StartInfo.FileName = "cmd.exe";
+            cmdProcess.StartInfo.UseShellExecute = false;  // 是否使用操作系统shell启动
+            cmdProcess.StartInfo.RedirectStandardInput = true;  // 接受来自调用程序的输入信息
+            cmdProcess.StartInfo.RedirectStandardOutput = true;  // 由调用程序获取输出信息
+            cmdProcess.StartInfo.RedirectStandardError = true;  // 重定向标准错误输出
+            cmdProcess.StartInfo.CreateNoWindow = true;  // 不显示程序窗口
+            cmdProcess.Start();  // 启动程序
+
+            //向cmd窗口发送输入信息
+            cmdProcess.StandardInput.WriteLine(strCMD + " &exit");
+
+            cmdProcess.StandardInput.AutoFlush = true;
+
+            //获取cmd窗口的输出信息
+            string error = cmdProcess.StandardError.ReadToEnd();
+            string output = cmdProcess.StandardOutput.ReadToEnd();
+            //等待程序执行完退出进程
+            cmdProcess.WaitForExit();
+            cmdProcess.Close();
+
+            if (error.Equals(""))
+            {
+                // 执行成功
+                return new CmdExecuteResultModel()
+                {
+                    StateCode = 0,
+                    ResultContent = output.Substring(output.IndexOf("&exit") + 7, output.Length - output.IndexOf("&exit") - 7)
+                };
+            }
+            else
+            {
+                // 执行失败
+                return new CmdExecuteResultModel()
+                {
+                    StateCode = 1,
+                    ResultContent = error
+                };
+            }
+        }
+    }
+}
--- StudioClient/Views/ManagePackagesWindow.xaml.cs
+++ StudioClient/Views/ManagePackagesWindow.xaml.cs
@@ -124,7 +124,7 @@ namespace StudioClient.Views
                     foreach (var tempSource in nuGetDefaultConfig.PackageSources)
                     {
                         strCMD = @".\Resources\NuGet\nuget.exe" + " list -Source \"" + tempSource.Value + "\"";
-                        executeResult = HandleExecuteCMD(strCMD);
+                        executeResult = ExecuteCMD.Handle(strCMD);
 
                         if (executeResult.StateCode == 0)
                         {
@@ -155,7 +155,7 @@ namespace StudioClient.Views
                     foreach (var tempSource in nuGetUserConfig.PackageSources)
                     {
                         strCMD = @".\Resources\NuGet\nuget.exe" + " list -Source \"" + tempSource.Value + "\"";
-                        executeResult = HandleExecuteCMD(strCMD);
+                        executeResult = ExecuteCMD.Handle(strCMD);
 
                         if (executeResult.StateCode == 0)
                         {
@@ -190,7 +190,7 @@ namespace StudioClient.Views
 
                     KeyValuePair<string, string> packageSources = (KeyValuePair<string, string>)selectedItem.Tag;
                     strCMD = @".\Resources\NuGet\nuget.exe" + " list -Source \"" + packageSources.Value + "\"";
-                    executeResult = HandleExecuteCMD(strCMD);
+                    executeResult = ExecuteCMD.Handle(strCMD);
 
                     if (executeResult.StateCode == 0)
                     {
@@ -224,54 +224,6 @@ namespace StudioClient.Views
         private void On_Left_Menu_Package_Sources_Selection_Changed(object sender, SelectionChangedEventArgs e)
         {
             
-        }
-
-        /// <summary>
-        /// 执行 CMD 命令
-        /// </summary>
-        /// <param name="strCMD"></param>
-        /// <returns></returns>
-        private CmdExecuteResultModel HandleExecuteCMD(string strCMD)
-        {
-            Process cmdProcess = new Process();
-            cmdProcess.StartInfo.FileName = "cmd.exe";
-            cmdProcess.StartInfo.UseShellExecute = false;  // 是否使用操作系统shell启动
-            cmdProcess.StartInfo.RedirectStandardInput = true;  // 接受来自调用程序的输入信息
-            cmdProcess.StartInfo.RedirectStandardOutput = true;  // 由调用程序获取输出信息
-            cmdProcess.StartInfo.RedirectStandardError = true;  // 重定向标准错误输出
-            cmdProcess.StartInfo.CreateNoWindow = true;  // 不显示程序窗口
-            cmdProcess.Start();  // 启动程序
-
-            //向cmd窗口发送输入信息
-            cmdProcess.StandardInput.WriteLine(strCMD + " &exit");
-
-            cmdProcess.StandardInput.AutoFlush = true;
-
-            //获取cmd窗口的输出信息
-            string error = cmdProcess.StandardError.ReadToEnd();
-            string output = cmdProcess.StandardOutput.ReadToEnd();
-            //等待程序执行完退出进程
-            cmdProcess.WaitForExit();
-            cmdProcess.Close();
-
-            if (error.Equals(""))
-            {
-                // 执行成功
-                return new CmdExecuteResultModel()
-                {
-                    StateCode = 0,
-                    ResultContent = output.Substring(output.IndexOf("&exit") + 7, output.Length - output.IndexOf("&exit") - 7)
-                };
-            }
-            else
-            {
-                // 执行失败
-                return new CmdExecuteResultModel()
-                {
-                    StateCode = 1,
-                    ResultContent = error
-                };
-            }
         }
 
         /// <summary>
@@ -387,7 +339,7 @@ namespace StudioClient.Views
                 return false;
             }
             string strCMD = @".\Resources\NuGet\nuget.exe" + " list -Source \"" + _sourcePathTextBox.Text + "\"";
-            if (HandleExecuteCMD(strCMD).StateCode != 0)
+            if (ExecuteCMD.Handle(strCMD).StateCode != 0)
             {
                 MessageBox.Show("无法验证软件包源路径 “" + _sourcePathTextBox.Text + "”", "警告");
                 return false;
